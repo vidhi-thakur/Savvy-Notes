7cc5f5307ec7168bedee27f19c3e6556719eace1
Vidhi Thakur <72219676+vidhi-thakur@users.noreply.github.com>
2022-04-22 01:51:14 +0530

    created dashboard cards

--- src/components/dashboard/dashboardCard/DashboardCard.js
+++ src/components/dashboard/dashboardCard/DashboardCard.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './DashboardCard.css';
+//icons 
+import { VscPinned } from 'react-icons/vsc'
 
 function DashboardCard() {
   return (
-    <div>DashboardCard</div>
+    <div className='dashboardCard'>
+      <header className='dashboardCard-header'>
+        <h3>
+          Title of the note
+        </h3>
+        <VscPinned />
+      </header>
+
+    </div>
   )
 }
 
-export {DashboardCard}
+export { DashboardCard }
