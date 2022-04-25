bb00276ca45321531bd0d7ea50c4018cc446ad63
Vidhi Thakur <72219676+vidhi-thakur@users.noreply.github.com>
2022-04-26 02:10:16 +0530

    updated dashboard card

--- src/components/dashboard/dashboardCard/DashboardCard.js
+++ src/components/dashboard/dashboardCard/DashboardCard.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './DashboardCard.css';
 //icons 
-import { VscPinned } from 'react-icons/vsc'
+import { MdOutlineLabel } from 'react-icons/md';
+import { BsArchive, BsPinAngleFill, BsPinAngle } from 'react-icons/bs';
+import { FiTrash2 } from 'react-icons/fi';
 
 function DashboardCard() {
+  const [isCardPinned, setIsCardPinned] = useState(false)
   return (
     <div className='dashboardCard'>
       <header className='dashboardCard-header'>
         <h3>
           Title of the note
         </h3>
-        <VscPinned />
+        <div className='dashboardCard-pin' onClick={() => setIsCardPinned(!isCardPinned)}>
+          {isCardPinned ? <BsPinAngleFill /> : <BsPinAngle />}
+        </div>
       </header>
       <main className='ellipsis'>
         body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card body of the card
       </main>
+      <footer className='dashboardCard-bottomStrip'>
+        <MdOutlineLabel />
+        <BsArchive />
+        <FiTrash2 />
+      </footer>
 
     </div>
   )
