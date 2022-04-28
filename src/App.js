83a6fa2af4b49b3b60048a93cfff5990a3c63570
Vidhi Thakur <72219676+vidhi-thakur@users.noreply.github.com>
2022-04-29 00:26:36 +0530

    added requires auth component and login page

--- src/App.js
+++ src/App.js
@@ -5,19 +5,28 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom'
 
 //local components
-import { Dashboard, Home } from 'pages/pagesGlobalExport';
+import { Dashboard, Home, Login } from 'pages/pagesGlobalExport';
 import { Navbar, Sidebar } from 'components/componentExport';
+
+// helpers
+import RequiresAuth from 'helpers/auth/RequiresAuth';
 
 function App() {
   const [isSidearVisible, setIsSidearVisible] = useState(false)
+  const [isUserLoggenIn, setIsUserLoggenIn] = useState(true)
   return (
     <div className="App">
       <Navbar setIsSidearVisible={setIsSidearVisible} />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Sidebar isSidearVisible={isSidearVisible}>
-          <Dashboard />
-        </Sidebar>} />
+        <Route path="/dashboard" element={
+          <RequiresAuth login={isUserLoggenIn}>
+            <Sidebar isSidearVisible={isSidearVisible}>
+              <Dashboard />
+            </Sidebar>
+          </RequiresAuth>
+        } />
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<h1>Page not found</h1>} />
       </Routes>
     </div>
